d8ebae6e4d24e2cf1a4f343301b079b0014ee993
ninjacornix <milanvrankic@gmail.com>
2024-01-24 21:46:10 +0100

    test 1

--- hakiranje_vjestina/astro.config.mjs
+++ hakiranje_vjestina/astro.config.mjs
@@ -16,29 +16,85 @@ export default defineConfig({
       discord: 'https://discord.com/invite/E7ad4UGbrG'
     },
     sidebar: [{
-      label: 'O vještini',
+      label: 'Vještina',
+      collapsed: true,
       items: [{
-        label: 'Sadržaj',
-        link: '/lekcije/vjestina/o_vjestini'
+        label: 'O vještini',
+        link: '/lekcije/vjestina/o_vjestini',
+        badge: 'New'
       },{
         label: 'Predavanja',
-        link: '/lekcije/vjestina/predavanja'
+        link: '/lekcije/vjestina/predavanja',
+        badge: 'New'
       }, {
         label: 'Bodovanje',
-        link: '/lekcije/vjestina/bodovanje'
-      }]
-    },
-      {
-      label: 'O materijalima',
-      items: [{
-        label: 'Sadržaj',
-        link: '/lekcije/uvod/o_materijalima'
+        link: '/lekcije/vjestina/bodovanje',
+        badge: 'New'
+      }, {
+        label: 'O materijalima',
+        link: '/lekcije/vjestina/o_materijalima',
+        badge: 'New'
+      }, {
+        label: 'FAQ',
+        link: '/lekcije/vjestina/faq',
+        badge: 'New'
       }]
     }, {
-      label: 'Reference',
-      autogenerate: {
-        directory: 'reference'
-      }
+      label: 'OSINT',
+      collapsed: true,
+      items: [{
+        label: 'O OSINT-u',
+        link: '/lekcije/osint/o_osintu',
+      }]
+    }, {
+      label: 'Forenzika',
+      collapsed: true,
+      items: [{
+        label: 'O forenzici',
+        link: '/lekcije/forenzika/o_forenzici'
+      }]
+    }, {
+      label: 'Kriptografija',
+      collapsed: true,
+      items: [{
+        label: 'O kriptografiji',
+        link: '/lekcije/kriptografija/o_kriptografiji'
+      }]
+    }, {
+      label: 'Web',
+      collapsed: true,
+      items: [{
+        label: 'O webu',
+        link: '/lekcije/web/o_webu'
+      }]
+    }, {
+      label: 'Reverzno inženjerstvo',
+      collapsed: true,
+      items: [{
+        label: 'O reverznom inženjerstvu',
+        link: '/lekcije/rev/o_reversingu'
+      }]
+    }, {
+      label: 'Binary',
+      collapsed: true,
+      items: [{
+        label: 'O binary exploitation',
+        link: '/lekcije/bin/o_binaryju'
+      }]
+    }, {
+      label: 'Etičko hakiranje',
+      collapsed: true,
+      items: [{
+        label: 'O etičkom hakiranju',
+        link: '/lekcije/eticko/o_etickom_hakiranju'
+      }]
+    },{
+      label: 'Hardversko hakiranje',
+      collapsed: true,
+      items: [{
+        label: 'O hardverskom hakiranju',
+        link: '/lekcije/hardversko/o_hardverskom_hakiranju'
+      }]
     }]
   }), tailwind()]
 });
